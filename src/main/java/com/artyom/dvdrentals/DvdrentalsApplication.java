6d2342f269f78353d27702463fcfe836bb53ad4a
Artyom Khvediouk <artyomk@yahoo-inc.com>
2019-12-28 10:56:47 -0800

    First commit

---
+++ src/main/java/com/artyom/dvdrentals/DvdrentalsApplication.java
@@ -0,0 +1,13 @@
+package com.artyom.dvdrentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvdrentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artyom/dvdrentals/DvdrentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artyom.dvdrentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdrentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
